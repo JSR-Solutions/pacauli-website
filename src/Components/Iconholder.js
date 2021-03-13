4a6546811081f30a1d2f0c1b675e82d3eff73115
aditya <aditya29ks@gmail.com>
2021-03-13 15:59:10 -0500

    no idea

---
+++ src/Components/Iconholder.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Iconholder = () => {
+  return (
+    <div iconholder-god>
+      <div class="iconholder upar">Andar Dekho</div>
+      <div class="iconholder andar">Nikal Laude</div>
+    </div>
+  );
+};
+
+export default Iconholder;
--- src/Core/Home.js
+++ src/Core/Home.js
@@ -2,11 +2,12 @@ import React from "react";
 import Landingscreen from "../Components/Laningscreen";
 import Footer from "../Components/Footer";
 import Hometext from "../Components/Hometext";
+
 const Home = () => {
   return (
     <div>
       <Landingscreen />
-      
+     
 
       <Footer />
     </div>
