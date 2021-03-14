63e31af956baf4bd1d7a014092940df80d04afb9
aditya <aditya29ks@gmail.com>
2021-03-14 11:51:58 -0400

    akku bhai god

---
+++ src/Components/Horizontalscroll.js
@@ -0,0 +1,36 @@
+import React from "react";
+import "../Styles/Horizontalscroll.css";
+import{Row, Col, Container} from 'react-bootstrap'
+
+
+const Horizontalscroll = () => {
+  return (
+    <div className="hs-god">
+      <Container>
+        <Row>
+          <Col xl={12} >
+          Paris Saint-Germain’s board of directors is “very confident” that the club is in pole position to acquire the services of Barcelona captain Lionel Messi on a free transfer this summer, according to a report from Brazilian journalist Marcelo Bechler.
+          </Col>
+          <Col xl={12}>
+          Paris Saint-Germain’s board of directors is “very confident” that the club is in pole position to acquire the services of Barcelona captain Lionel Messi on a free transfer this summer, according to a report from Brazilian journalist Marcelo Bechler.
+          </Col>
+          <Col xl={12} >
+          Paris Saint-Germain’s board of directors is “very confident” that the club is in pole position to acquire the services of Barcelona captain Lionel Messi on a free transfer this summer, according to a report from Brazilian journalist Marcelo Bechler.
+          </Col>
+          <Col xl={12} >
+          Paris Saint-Germain’s board of directors is “very confident” that the club is in pole position to acquire the services of Barcelona captain Lionel Messi on a free transfer this summer, according to a report from Brazilian journalist Marcelo Bechler.
+          </Col>
+          <Col xl={12} >
+          Paris Saint-Germain’s board of directors is “very confident” that the club is in pole position to acquire the services of Barcelona captain Lionel Messi on a free transfer this summer, according to a report from Brazilian journalist Marcelo Bechler.
+          </Col>
+          <Col xl={12} >
+          Paris Saint-Germain’s board of directors is “very confident” that the club is in pole position to acquire the services of Barcelona captain Lionel Messi on a free transfer this summer, according to a report from Brazilian journalist Marcelo Bechler.
+          </Col>
+        </Row>
+      </Container>
+
+    </div>
+  );
+};
+
+export default Horizontalscroll;
