26c63c57b8f611963916c2046938981e34a18bd4
Chitransh Jain <cjsmcs2810@gmail.com>
2021-03-16 18:14:51 +0530

    category strip fixed

--- src/Components/CategoryStrip.js
+++ src/Components/CategoryStrip.js
@@ -1,21 +1,34 @@
+import { Button } from "react-bootstrap";
 import React from "react";
-import "../Styles/catStrip.css"
+import "../Styles/catStrip.css";
 
-function catStrip(props){
-    let catStripCss={
-        background:`url(${props.img})`,
-        width: "100%",
-        height: "400px",
-        backgroundRepeat: "no-repeat",
-        backgroundSize:"cover",
-        backgroundPosition:"center",
-        paddingTop:"150px" 
-    }
-    return(
-        <div style={catStripCss}>
-            <h1>{props.Name}</h1>
-        </div>
+function catStrip(props) {
+  let catStripCss = {
+    background: `linear-gradient(45deg, rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)), url(${props.img})`,
+    width: "100%",
+    height: "100%",
+    backgroundRepeat: "no-repeat",
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    flexDirection: "column",
+    transition: "0.5s ease-in-out",
+  };
 
-    );
+  const buttonClick = () => {};
+  return (
+    <div className="category-strip-main">
+      <div className="category-strip" style={catStripCss}></div>
+      <div className="category-strip-content">
+        <h1 className="category-title">{props.Name}</h1>
+        <p className="category-quote">{props.quote}</p>
+        <Button className="category-button" onClick={buttonClick}>
+          View Packages
+        </Button>
+      </div>
+    </div>
+  );
 }
 export default catStrip;
--- src/Core/Categories.js
+++ src/Core/Categories.js
@@ -1,52 +1,75 @@
-import React from 'react';
+import React from "react";
 import Header from "../Components/Header";
 import Footer from "../Components/Footer";
-import "../Styles/categoryPage.css"
-import Skiing from "../Assets/skiing.jpg"
-import Rock from "../Assets/rockClimbing.jpg"
-import Trekking from "../Assets/trekking.jpg"
-import Rafting from "../Assets/Rafting.jpg"
-import Camping from "../Assets/camping.jpg"
-import Expedition from "../Assets/expedition.jpg"
-import Chardham from "../Assets/chardham.jpg"
-import Cycling from "../Assets/cycling.jpg"
-import {Row,Col} from "react-bootstrap"
-import CatStrip from "../Components/CategoryStrip"
-
-
+import "../Styles/categoryPage.css";
+import Skiing from "../Assets/skiing.jpg";
+import Rock from "../Assets/rockClimbing.jpg";
+import Trekking from "../Assets/trekking.jpg";
+import Rafting from "../Assets/Rafting.jpg";
+import Camping from "../Assets/camping.jpg";
+import Expedition from "../Assets/expedition.jpg";
+import Chardham from "../Assets/chardham.jpg";
+import Cycling from "../Assets/cycling.jpg";
+import CatStrip from "../Components/CategoryStrip";
 
 const Categories = () => {
-const category=[{Name: "Skiing", img:Skiing},
-                {Name: "Trekking",img:Trekking},
-                {Name:"Rock Climbing", img:Rock},
-                {Name:"Cycling", img:Cycling},
-                {Name:"Camping", img:Camping},
-                {Name:"Rafting", img:Rafting},
-                {Name:"Expediton", img:Expedition},
-                {Name:"CharDham", img:Chardham},
-];
+  const category = [
+    {
+      Name: "Skiing",
+      img: Skiing,
+      quote: "“A pair of skis are the ultimate transportation to freedom.”",
+    },
+    {
+      Name: "Trekking",
+      img: Trekking,
+      quote: "“Every mountain top is within reach if you just keep climbing.”",
+    },
+    {
+      Name: "Rock Climbing",
+      img: Rock,
+      quote: "“The best view comes after the hardest climb.”",
+    },
+    {
+      Name: "Cycling",
+      img: Cycling,
+      quote: "“Nothing compares to the simple pleasure of riding a bike”",
+    },
+    {
+      Name: "Camping",
+      img: Camping,
+      quote: "“The stars were better company anyway”",
+    },
+    {
+      Name: "Rafting",
+      img: Rafting,
+      quote:
+        "“The first river you paddle runs through the rest of your life. It bubbles up in pools and eddies to remind you who you are.”",
+    },
+    {
+      Name: "Expediton",
+      img: Expedition,
+      quote: "“Life is either a daring adventure or nothing.”",
+    },
+    {
+      Name: "CharDham",
+      img: Chardham,
+      quote: "“Chardham yatra washes away all sins and opens doors for Moksha”",
+    },
+  ];
 
-    return (
-        <div>
-
-            <Header />
-            <div className="heading-categories">
-          <p>&nbsp;&nbsp;Categories&nbsp;&nbsp;</p>
-        </div>
-            <div className="catt">
-            {category.map((cat) =>{
-                return(
-                <CatStrip Name={cat.Name} img={cat.img} />
-                )
-            })
-        }
-        </div>
-        <Footer/>
-            
-</div>
-
-
-            
-    );
-}
+  return (
+    <div>
+      <Header />
+      <div className="heading-categories">
+        <p>&nbsp;&nbsp;Categories&nbsp;&nbsp;</p>
+      </div>
+      <div className="catt">
+        {category.map((cat) => {
+          return <CatStrip Name={cat.Name} img={cat.img} quote={cat.quote} />;
+        })}
+      </div>
+      <Footer />
+    </div>
+  );
+};
 export default Categories;
