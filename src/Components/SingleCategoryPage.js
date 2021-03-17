4195be5ba9a347c92c8d9381accec82d82a0a087
Chitransh Jain <cjsmcs2810@gmail.com>
2021-03-17 11:48:57 +0530

    made some modifications

--- src/Components/SingleCategoryPage.js
+++ src/Components/SingleCategoryPage.js
@@ -1,14 +1,15 @@
-import  React from "react"
-import "../Styles/SingleCatScreen.css"
-import category from "../helper/categoryData"
-import {Row,Col} from "react-bootstrap"
-import Card from "../Components/Card3"
+import React from "react";
+import "../Styles/SingleCatScreen.css";
+import category from "../helper/categoryData";
+import { Row, Col } from "react-bootstrap";
+import Card from "../Components/Card3";
 
-function SingleCatScreen(props){
-    const headingStyle={
-    
-    background:`linear-gradient(45deg, rgba(44, 44, 44, 0.25), rgba(44, 44, 44, 0.25)),url(${category[props.index].img})`,
-    height: "700px",
+function SingleCatScreen(props) {
+  const headingStyle = {
+    background: `linear-gradient(45deg, rgba(44, 44, 44, 0.25), rgba(44, 44, 44, 0.25)),url(${
+      category[props.index].img
+    })`,
+    height: "600px",
     width: "100%",
     backgroundPosition: "center",
     backgroundSize: "cover",
@@ -17,44 +18,43 @@ function SingleCatScreen(props){
     justifyContent: "center",
     alignItems: "center",
     textAlign: "center",
-    boxShadow: "10px 10px 80px 10px rgba(44, 44, 44, 0.45)"
-    }
-    return(
-        <div>
-        <div style={headingStyle} className="heading-catSreen"  >
-          <p>&nbsp;&nbsp;{category[props.index].Name}&nbsp;&nbsp;</p>
-        </div>
-        <div className="h_cardDiv">
+    boxShadow: "10px 10px 80px 10px rgba(44, 44, 44, 0.45)",
+  };
+  return (
+    <div>
+      <div style={headingStyle} className="heading-catSreen">
+        <p>&nbsp;&nbsp;{category[props.index].Name}&nbsp;&nbsp;</p>
+      </div>
+      <div className="category-quote-div">
+        <p className="category-quote">{category[props.index].quote}</p>
+        <p className="category-line">Have a look at our {category[props.index].Name} packages for a wonderful and memorable {category[props.index].Name} experience.</p>
+      </div>
+      <div className="h_cardDiv">
         <Row>
-        <Col>
-        <Card/>
-        </Col>
-        <Col>
-        <Card/>
-        </Col>
-        <Col>
-        <Card/>
-        </Col>
-        <Col>
-        <Card/>
-        </Col>
-        <Col>
-        <Card/>
-        </Col>
-        <Col>
-        <Card/>
-        </Col>
-        <Col>
-        <Card/>
-        </Col>
-        
+          <Col>
+            <Card />
+          </Col>
+          <Col>
+            <Card />
+          </Col>
+          <Col>
+            <Card />
+          </Col>
+          <Col>
+            <Card />
+          </Col>
+          <Col>
+            <Card />
+          </Col>
+          <Col>
+            <Card />
+          </Col>
+          <Col>
+            <Card />
+          </Col>
         </Row>
-        
-        </div>
-        
-
-        </div>
-    )
-
+      </div>
+    </div>
+  );
 }
 export default SingleCatScreen;
